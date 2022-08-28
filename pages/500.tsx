db71a1d6e87d4ac025493ecbafca6791418bd1f5
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2022-08-28 23:59:44 +0700

    :lipstick: custom error page

---
+++ pages/500.tsx
@@ -0,0 +1,23 @@
+import { GetStaticProps, NextPage } from 'next'
+
+export const getStaticProps: GetStaticProps = async () => {
+  return { props: {} }
+}
+
+const Custom500: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-96">
+      <div className="space-y-4 md:space-y-2">
+        <div className="flex items-baseline space-x-4">
+          <span role="img" aria-label="see-no-evil-monkey" className="text-5xl">
+            🙊
+          </span>
+          <h1 className="text-4xl font-bold leading-[3rem]">500</h1>
+        </div>
+        <p className="leading-7">Oops, something went wrong!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Custom500
