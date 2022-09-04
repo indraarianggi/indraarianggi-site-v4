d509d576101250405e851808e0472fc09da04733
Indra Arianggi Suryaatmaja <indraarianggi@gmail.com>
2022-09-04 18:57:08 +0700

    :mag: SEO for error pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,22 +1,35 @@
 import { GetStaticProps, NextPage } from 'next'
 
+import { PageSEO } from '@components/SEO'
+import siteMetadata from '@data/siteMetadata'
+
 export const getStaticProps: GetStaticProps = async () => {
   return { props: {} }
 }
 
 const Custom404: NextPage = () => {
   return (
-    <div className="flex items-center justify-center h-96">
-      <div className="space-y-4 md:space-y-2">
-        <div className="flex items-baseline space-x-4">
-          <span role="img" aria-label="see-no-evil-monkey" className="text-5xl">
-            🙈
-          </span>
-          <h1 className="text-4xl font-bold leading-[3rem]">404</h1>
+    <>
+      <PageSEO
+        title={`404 - ${siteMetadata.title}`}
+        description={siteMetadata.description}
+      />
+
+      <div className="flex items-center justify-center h-96">
+        <div className="space-y-4 md:space-y-2">
+          <div className="flex items-baseline space-x-4">
+            <span
+              role="img"
+              aria-label="see-no-evil-monkey"
+              className="text-5xl">
+              🙈
+            </span>
+            <h1 className="text-4xl font-bold leading-[3rem]">404</h1>
+          </div>
+          <p className="leading-7">Sorry, we couldn&apos;t find your page.</p>
         </div>
-        <p className="leading-7">Sorry, we couldn&apos;t find your page.</p>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,22 +1,35 @@
 import { GetStaticProps, NextPage } from 'next'
 
+import { PageSEO } from '@components/SEO'
+import siteMetadata from '@data/siteMetadata'
+
 export const getStaticProps: GetStaticProps = async () => {
   return { props: {} }
 }
 
 const Custom500: NextPage = () => {
   return (
-    <div className="flex items-center justify-center h-96">
-      <div className="space-y-4 md:space-y-2">
-        <div className="flex items-baseline space-x-4">
-          <span role="img" aria-label="see-no-evil-monkey" className="text-5xl">
-            🙊
-          </span>
-          <h1 className="text-4xl font-bold leading-[3rem]">500</h1>
+    <>
+      <PageSEO
+        title={`404 - ${siteMetadata.title}`}
+        description={siteMetadata.description}
+      />
+
+      <div className="flex items-center justify-center h-96">
+        <div className="space-y-4 md:space-y-2">
+          <div className="flex items-baseline space-x-4">
+            <span
+              role="img"
+              aria-label="see-no-evil-monkey"
+              className="text-5xl">
+              🙊
+            </span>
+            <h1 className="text-4xl font-bold leading-[3rem]">500</h1>
+          </div>
+          <p className="leading-7">Oops, something went wrong!</p>
         </div>
-        <p className="leading-7">Oops, something went wrong!</p>
       </div>
-    </div>
+    </>
   )
 }
 
